c2709a90cb11166b4be29093d435c050a3b0da2c
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-14 00:56:04 +0600

    Saving to GitHub

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from .utils import *
 def store(request):
     products = Product.objects.all()
     context = {'products': products}
@@ -12,9 +13,17 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
     context = {'items': items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order':order}
     return render(request, 'store/checkout.html', context)
