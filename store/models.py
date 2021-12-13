78526be39e2d7bab0452db07c5ef286f6941e1e8
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-13 15:42:53 +0600

    Saving to GitHub

--- store/models.py
+++ store/models.py
@@ -15,6 +15,15 @@ class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(null = True, blank = True)
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
 	def __str__(self):
 		return self.name
