a9137aa91c545526d92a4359843b820856714c6f
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-18 00:51:12 +0600

    Saving to GitHub

--- store/models.py
+++ store/models.py
@@ -11,11 +11,15 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+
 class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
-	image = models.ImageField(null = True, blank = True)
+	image = models.ImageField(null=True, blank=True)
+
+	def __str__(self):
+		return self.name
 
 	@property
 	def imageURL(self):
@@ -25,9 +29,6 @@ class Product(models.Model):
 			url = ''
 		return url
 
-	def __str__(self):
-		return self.name
-
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
 	date_ordered = models.DateTimeField(auto_now_add=True)
@@ -36,21 +37,21 @@ class Order(models.Model):
 
 	def __str__(self):
 		return str(self.id)
-
+		
 	@property
-	def delivery(self):
-		delivery = False
+	def shipping(self):
+		shipping = False
 		orderitems = self.orderitem_set.all()
 		for i in orderitems:
 			if i.product.digital == False:
-				delivery = True
-		return delivery
+				shipping = True
+		return shipping
 
 	@property
 	def get_cart_total(self):
 		orderitems = self.orderitem_set.all()
 		total = sum([item.get_total for item in orderitems])
-		return total
+		return total 
 
 	@property
 	def get_cart_items(self):
@@ -63,12 +64,12 @@ class OrderItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
-    
+
 	@property
 	def get_total(self):
 		total = self.product.price * self.quantity
 		return total
- 
+
 class DeliveryAddress(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
